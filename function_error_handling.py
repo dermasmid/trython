24c4b8ec8c812201faaf62f11f5879195d9230c9
beni <beni.gutman13@gmail.com>
2021-06-06 20:36:52 +0300

    make it use as a decorator

--- function_error_handling.py
+++ function_error_handling.py
@@ -1,21 +1,22 @@
 import time
+import functools
 
 __version__ = '1.2'
 
 
-def wrap(func, times_to_try: int = 5, time_to_sleep: int = 30, errors_to_catch: tuple = (Exception), callback = None):
-    def error_handle(*args, **kwargs):
-        print(';ddd')
-        times = 0
-        while True:
-            try:
-                return func(*args, **kwargs)
-            except errors_to_catch as e:
-                if not times > times_to_try:
+def handle_error(number_of_attempts: int = 5,
+                 time_to_sleep: int = 30,
+                 errors_to_catch: tuple = (Exception, ),
+                 callback=None):
+    def handle_error_decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for attempt_number in range(number_of_attempts + 1):
+                try:
+                    return func(*args, **kwargs)
+                except errors_to_catch as ex:
                     time.sleep(time_to_sleep)
-                    times += 1
-                else:
-                    if callback:
-                        callback(e)
-                    raise e
-    return error_handle
+            if callback:
+                callback(ex)
+            raise ex
+        return wrapper
